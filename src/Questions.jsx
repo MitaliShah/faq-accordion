93bc0069b3e400979a39b13c7a3c2990ec662b8c
MitaliShah <mitalishaleenshah@gmail.com>
2023-12-05 14:04:57 -0600

    desktop changes title size and margin

--- src/Questions.jsx
+++ src/Questions.jsx
@@ -27,11 +27,21 @@ const IMG = styled.img`
     width: 1.5rem;
     height: 1.5rem;
     margin-right: 1.5rem;
+
+    @media (min-width: 37.5rem) {
+        width: 2.5rem;
+        height: 2.5rem;
+    }
 `;
 
 const Heading = styled.h1`
     color: #301534;
     font-family: Work Sans;
     font-size: 2rem;
-    font-weight: 700;   
+    font-weight: 700;
+
+    @media (min-width: 37.5rem) {
+        font-size: 3.5rem;
+        margin-bottom: 0.5rem;
+    }
 `;
